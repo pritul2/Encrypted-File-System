5ae1c4728e277a6aef80f38d1a3cc7a3b731e96d
pritul dave <41751718+pritul2@users.noreply.github.com>
2023-03-03 17:09:07 -0600

    Completed file length

--- EFS.java
+++ EFS.java
@@ -5,14 +5,10 @@
  * @email pritul.dave@utdallas.edu
  */
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.spec.KeySpec;
@@ -22,11 +18,12 @@ public class EFS extends Utility {
 
     private static final int USER_NAME_LENGTH = 128;
     private static final int PWD_LENGTH = 32;
-    private static final int HEADER_LENGTH = USER_NAME_LENGTH+PWD_LENGTH;
+    private static final int SALT_LENGTH = 16;
+    private static final int HEADER_LENGTH = USER_NAME_LENGTH + PWD_LENGTH + SALT_LENGTH;
     private static final int SECRET_DATA_LENGTH = 128;
     private static final int METADATA_LENGTH = HEADER_LENGTH + SECRET_DATA_LENGTH;
     private static final int HMAC_LENGTH = 32;
-
+   
 
     public EFS(Editor e) {
         super(e);
@@ -64,6 +61,11 @@ public class EFS extends Utility {
         return bf.array();
     }
 
+    private long bytesToLong(byte[] bytes) {
+        ByteBuffer bf = ByteBuffer.wrap(bytes);
+        return bf.getLong();
+    }
+
     private byte[] padToMultiple(byte[] data, int blockSize) {
         int padding = blockSize - (data.length % blockSize);
         byte[] paddedData = new byte[data.length + padding];
@@ -83,6 +85,22 @@ public class EFS extends Utility {
         return key;
     }
 
+    private byte[] getPasswordHash(String password, byte[] salt) throws Exception{
+        // Padding password salt
+        String padded_password = padString(password, 128);
+    
+        byte[] passwordBytes = padded_password.getBytes();
+    
+        byte[] saltedPasswordBytes = new byte[passwordBytes.length + salt.length];
+    
+        System.arraycopy(passwordBytes, 0, saltedPasswordBytes, 0, passwordBytes.length);
+        System.arraycopy(salt, 0, saltedPasswordBytes, passwordBytes.length, salt.length);
+    
+        byte[] hashed_pwd = hash_SHA256(saltedPasswordBytes);
+    
+        return hashed_pwd;
+    }
+
     @Override
     public void create(String file_name, String user_name, String password) throws Exception {
 
@@ -96,36 +114,36 @@ public class EFS extends Utility {
         File metadata_file = new File(dir, "0");
         metadata_file.createNewFile();
 
-        // Generating a random 16B salt to add to password
+        // Generting salt
         byte[] salt = secureRandomNumber(16);
+        System.out.println("Salt length "+salt.length);
+        System.out.println("Salt  "+new String(salt));
 
-        // Padding username and password salt
+        // Padding username
         String padded_user_name = padString(user_name, 128);
-        String padded_password = padString(password, 128);
-        byte[] passwordBytes = padded_password.getBytes();
-        byte[] saltedPasswordBytes = new byte[passwordBytes.length + salt.length];
-        System.arraycopy(passwordBytes, 0, saltedPasswordBytes, 0, passwordBytes.length);
-        System.arraycopy(salt, 0, saltedPasswordBytes, passwordBytes.length, salt.length);
-        byte[] hashed_pwd = hash_SHA256(saltedPasswordBytes);
 
+        // Making hashed password
+        byte[] hashed_pwd = getPasswordHash(password,salt);
+        System.out.println(new String(hashed_pwd));
         // Adding padded username and password salt to header
-        byte[] header = new byte[padded_user_name.length() + hashed_pwd.length];
+        byte[] header = new byte[padded_user_name.length() + hashed_pwd.length + salt.length];
         System.arraycopy(padded_user_name.getBytes(), 0, header, 0, padded_user_name.length());
         System.arraycopy(hashed_pwd, 0, header, padded_user_name.length(), hashed_pwd.length);
+        System.arraycopy(salt, 0, header, padded_user_name.length() + hashed_pwd.length, salt.length);
 
-        //System.out.println("Padded user name"+ padded_user_name.length());
-        //System.out.println("Salted password"+saltedPasswordBytes.length);
-        //System.out.println("Hashed password"+hashed_pwd.length);
-        //System.out.println("header size"+header.length);
-
+        // System.out.println("Padded user name"+ padded_user_name.length());
+        // System.out.println("Salted password"+saltedPasswordBytes.length);
+        // System.out.println("Hashed password"+hashed_pwd.length);
+        // System.out.println("header size"+header.length);
+        System.out.println("Meta data file "+metadata_file.length());
         byte[] file_length = longToBytes(metadata_file.length());
 
-        //System.out.println("file_length size"+file_length.length);
+        // System.out.println("file_length size"+file_length.length);
 
         // Creating secret data array of length pwd_hash + file_length
         byte[] secret_data = new byte[hashed_pwd.length + file_length.length];
 
-        //System.out.println("secret_data size"+secret_data.length);
+        // System.out.println("secret_data size"+secret_data.length);
 
         // Adding password hash and File length to secret data
         System.arraycopy(hashed_pwd, 0, secret_data, 0, hashed_pwd.length);
@@ -134,23 +152,23 @@ public class EFS extends Utility {
         // Padding the secret data
         secret_data = padToMultiple(secret_data, 128);
 
-        //System.out.println("Secret data length"+ secret_data.length);
+        // System.out.println("Secret data length"+ secret_data.length);
 
         // Generating key on the basis of password
-        byte[] pwd_base_key = deriveKeyFromPassword(padded_password, salt);
-
+        byte[] pwd_base_key = deriveKeyFromPassword(padString(password, 128), salt);
+        System.out.println("Key "+new String(pwd_base_key));
         // Encrypting secret data
         byte[] encrypted_secret_data = encript_AES(secret_data, pwd_base_key);
 
-        //System.out.println("Encrypted Secret data length"+ encrypted_secret_data.length);
+        // System.out.println("Encrypted Secret data length"+
+        // encrypted_secret_data.length);
 
         // Adding header and encrypted secret data to metadata
         byte[] metadata = new byte[header.length + encrypted_secret_data.length];
         System.arraycopy(header, 0, metadata, 0, header.length);
         System.arraycopy(encrypted_secret_data, 0, metadata, header.length, encrypted_secret_data.length);
 
-        //System.out.println("metadata length"+ metadata.length);
-
+        // System.out.println("metadata length"+ metadata.length);
 
         // Computing HMAC on metadata
         byte[] hmac = hash_SHA256(metadata);
@@ -158,8 +176,8 @@ public class EFS extends Utility {
         // Writing metadata and HMAC to file
         FileOutputStream metadata_output = new FileOutputStream(metadata_file);
 
-        //System.out.println("metadata length"+ metadata.length);
-        //System.out.println("hmac length"+ hmac.length);
+        // System.out.println("metadata length"+ metadata.length);
+        // System.out.println("hmac length"+ hmac.length);
 
         metadata_output.write(metadata);
         metadata_output.write("\n".getBytes());
@@ -169,7 +187,28 @@ public class EFS extends Utility {
 
         return;
     }
+    private boolean validate_HMAC(byte[] metadata){
 
+        // Checking if the length of the metadata file is valid
+        if (metadata.length != HEADER_LENGTH + SECRET_DATA_LENGTH +  HMAC_LENGTH + 1) {
+            throw new Exception("Invalid File length");
+        }
+
+        // Extracting metadata bytes and HMAC from metadata file
+        byte[] metadata_bytes = new byte[METADATA_LENGTH];
+        byte[] hmac = new byte[HMAC_LENGTH];
+        FileInputStream read_metadata = new FileInputStream(meta);
+        read_metadata.read(metadata_bytes);
+        read_metadata.skip(1);
+        read_metadata.read(hmac);
+        read_metadata.close();
+
+        // Computing HMAC of metadata bytes
+        byte[] computed_hmac = hash_SHA256(metadata_bytes);
+
+        return Arrays.equals(hmac, computed_hmac);
+
+    }
     /**
      * Steps to consider...
      * <p>
@@ -181,37 +220,30 @@ public class EFS extends Utility {
 
     @Override
     public String findUser(String file_name) throws Exception {
+        // Creating file objects for the file and its metadata
         File file = new File(file_name);
         File meta = new File(file, "0");
+
+        // Reading the metadata file into a byte array
         FileInputStream metadata_input = new FileInputStream(meta);
+
         byte[] metadata = metadata_input.readAllBytes();
 
-        if (metadata.length != HEADER_LENGTH+SECRET_DATA_LENGTH+HMAC_LENGTH+1) {
-            throw new Exception("Invalid File length");
-        }
-
-        byte[] metadata_bytes = new byte[METADATA_LENGTH];
-        byte[] hmac = new byte[HMAC_LENGTH];
-        FileInputStream read_metadata = new FileInputStream(meta);
-        read_metadata.read(metadata_bytes);
-        read_metadata.skip(1);
-        read_metadata.read(hmac);
-        read_metadata.close();
-
-        byte[] computed_hmac = hash_SHA256(metadata_bytes);
-
-        if (!Arrays.equals(hmac, computed_hmac)) {
+        // Checking if the computed HMAC matches with the HMAC from metadata file
+        if (!validate_HMAC(metadata)) {
             throw new Exception("Metadata file has been tampered with!");
         }
 
         metadata_input.close();
 
+        // Extracting the user name from metadata
         byte[] user_name_bytes = Arrays.copyOfRange(metadata, 0, 128);
-
         String user_name = new String(user_name_bytes).trim();
 
+        // Printing the user name and returning it
         System.out.println(user_name);
-        return(user_name);
+        return user_name;
+
     }
 
     /**
@@ -225,9 +257,68 @@ public class EFS extends Utility {
      * <p>
      * - decrypt file length out of encrypted secret data
      */
+    private long convertToLong(byte[] bytes)
+    {
+        long value = 0l;
+ 
+        // Iterating through for loop
+        for (byte b : bytes) {
+            // Shifting previous value 8 bits to right and
+            // add it with next value
+            value = (value << 8) + (b & 255);
+        }
+ 
+        return value;
+    }
+
+    private boolean verify_pwd(byte[] metadata, String password) throws Exception{
+        // Deriving the salt from header
+        byte[] salt = Arrays.copyOfRange(metadata, USER_NAME_LENGTH+PWD_LENGTH, HEADER_LENGTH);
+
+        byte[] encrypted_secret_data = Arrays.copyOfRange(metadata, HEADER_LENGTH, METADATA_LENGTH);
+
+        // Getting hashed password
+        byte[] hashed_pwd_2 = getPasswordHash(password,salt);
+
+        // Getting password base key
+        byte[] pwd_base_key = deriveKeyFromPassword(padString(password, 128), salt);
+
+        // Decrypting the secret data
+        byte[] secret_data = decript_AES(encrypted_secret_data, pwd_base_key);
+
+        // Getting the hashed password1 and password 2
+        byte[] hashed_pwd = Arrays.copyOfRange(secret_data, 0, PWD_LENGTH);
+
+        return Arrays.equals(hashed_pwd, hashed_pwd_2);
+        
+    }
+
     @Override
     public int length(String file_name, String password) throws Exception {
-        return 0;
+
+        // Reading the meta data file
+        File dir = new File(file_name);
+        File metadata_file = new File(dir, "0");
+        FileInputStream metadata_input = new FileInputStream(metadata_file);
+        byte[] metadata = metadata_input.readAllBytes();
+
+        //Verify HMAC
+        if (!validate_HMAC(metadata)) {
+            throw new Exception("Metadata file has been tampered with!");
+        }
+
+        //Comparing both the hashed passwords
+        if (!verify_pwd(metadata,password)){
+            throw new Exception("Password Does not Match");
+        }
+
+        // Decrypting the file length
+        long file_length = bytesToLong(Arrays.copyOfRange(secret_data, PWD_LENGTH, secret_data.length));
+        
+        System.out.println(file_length);
+
+        return (int)file_length;
+
     }
 
     /**
@@ -241,6 +332,23 @@ public class EFS extends Utility {
      */
     @Override
     public byte[] read(String file_name, int starting_position, int len, String password) throws Exception {
+        File dir = new File(file_name);
+        File metadata_file = new File(dir, "0");
+        FileInputStream metadata_input = new FileInputStream(metadata_file);
+        byte[] metadata = metadata_input.readAllBytes();
+        
+        //Verify HMAC
+        if (!validate_HMAC(metadata)) {
+            throw new Exception("Metadata file has been tampered with!");
+        }
+
+        //Comparing both the hashed passwords
+        if (!verify_pwd(metadata,password)){
+            throw new Exception("Password Does not Match");
+        }
+
+
+        
         return null;
     }
 
@@ -257,6 +365,19 @@ public class EFS extends Utility {
      */
     @Override
     public void write(String file_name, int starting_position, byte[] content, String password) throws Exception {
+        File dir = new File(file_name);
+        File metadata_file = new File(dir, "0");
+        FileInputStream metadata_input = new FileInputStream(metadata_file);
+        byte[] metadata = metadata_input.readAllBytes();
+
+        //Finding file length and verifing the password
+        int file_length = length(file_name,password);
+
+
+        if ( (starting_position < 0 || starting_position >= file_length) || (len < 0 || starting_position + len > file_length) {
+            throw new IllegalArgumentException("Invalid starting position");
+        }
+
     }
 
     /**
@@ -291,12 +412,12 @@ public class EFS extends Utility {
         Editor edr = new Editor();
         EFS efs = new EFS(edr);
         try {
-            efs.create("my_file2","HelloWORLD","macbook");
-            efs.findUser("my_file2");
+            efs.create("my_file2", "HelloWORLD", "macbook");
+            System.out.println(efs.findUser("my_file2"));
+            System.out.println(efs.length("my_file2","macbook"));
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
-
 
     }
 
